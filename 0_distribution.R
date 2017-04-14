8376efc4de874971962d7d6b402ea8adc2ebd836
wen-jie.tseng <wenjietseng@gmail.com>
2017-04-15 00:01:02 +0800

    Add files via upload

---
+++ 0_distribution.R
@@ -0,0 +1,92 @@
+# static plots
+par(mfcol = c(3, 2))
+# 1
+curve(dnorm(x, mean = -2, sd = 1),
+      xlim = c(-5, 5), ylim = c(0, 0.5),
+      xlab = "X", ylab = "P(X)")
+abline(v = -2, lty = 2)
+text(3.5, 0.45, expression(paste(mu,"=-2, ",sigma,"=1")))
+
+# 2
+curve(dnorm(x, mean = -1, sd = 1),
+      xlim = c(-5, 5), ylim = c(0, 0.5),
+      xlab = "X", ylab = "P(X)")
+abline(v = -1, lty = 2)
+text(3.5, 0.45, expression(paste(mu,"=-1, ",sigma,"=1")))
+
+# 3
+curve(dnorm(x, mean = 0, sd = 1),
+      xlim = c(-5, 5), ylim = c(0, 0.5),
+      xlab = "X", ylab = "P(X)")
+abline(v = 0, lty = 2)
+text(3.5, 0.45, expression(paste(mu,"=0, ",sigma,"=1")))
+
+# 4
+curve(dnorm(x, mean = 0, sd = 1),
+      xlim = c(-5, 5), ylim = c(0, 0.5),
+      xlab = "X", ylab = "P(X)")
+abline(v = 0, lty = 2)
+text(3.5, 0.45, expression(paste(mu,"=0, ",sigma,"=1")))
+
+# 5
+curve(dnorm(x, mean = 0, sd = 2),
+      xlim = c(-5, 5), ylim = c(0, 0.5),
+      xlab = "X", ylab = "P(X)")
+abline(v = 0, lty = 2)
+text(3.5, 0.45, expression(paste(mu,"=0, ",sigma,"=2")))
+
+# 6
+curve(dnorm(x, mean = 0, sd = 3),
+      xlim = c(-5, 5), ylim = c(0, 0.5),
+      xlab = "X", ylab = "P(X)")
+abline(v = 0, lty = 2)
+text(3.5, 0.45, expression(paste(mu,"=0, ",sigma,"=3")))
+
+# animation
+library(animation)
+oopt <- ani.options(interval = 52, nmax = 630)
+mu <- c(seq(from = -2, to = 0, by = 0.016),
+        seq(from = 0, to = 2, by = 0.016),
+        seq(from = 2, to = 0, by = -0.016),
+        rep(0, 126),
+        rep(0, 126))
+sd <- c(rep(1, 126),
+        rep(1, 126),
+        rep(1, 126),
+        seq(from = 1, to = 3, by = 0.016),
+        seq(from = 3, to = 1, by = -0.016))
+
+for(i in 1:ani.options("nmax")) {
+  curve(dnorm(x, mean = mu[i], sd = sd[i]),
+        xlim = c(-5, 5), ylim = c(0, 0.5),
+        xlab = "X", ylab = "P(X)")
+  abline(v = mu[i], lty = 2)
+}
+
+#####
+
+# **Animation: normal distribution**
+```{r N_dist, echo = FALSE, fig.show = 'animate', out.width = '4in'}
+library(animation)
+oopt <- ani.options(nmax = 255)
+mu <- c(seq(from = -2, to = 0, by = 0.04),
+        seq(from = 0, to = 2, by = 0.04),
+        seq(from = 2, to = 0, by = -0.04),
+        rep(0, 51),
+        rep(0, 51))
+sd <- c(rep(1, 51),
+        rep(1, 51),
+        rep(1, 51),
+        seq(from = 1, to = 3, by = 0.04),
+        seq(from = 3, to = 1, by = -0.04))
+
+for(i in 1:ani.options("nmax")) {
+  curve(dnorm(x, mean = mu[i], sd = sd[i]),
+        xlim = c(-5, 5), ylim = c(0, 0.5),
+        xlab = "X", ylab = "P(X)")
+  abline(v = mu[i], lty = 2)
+}
+
+```
+
+
---
+++ 2_ggvis_eg.R
@@ -0,0 +1,73 @@
+library(ggvis)
+library(magrittr) # %>%
+library(dplyr)
+
+dta <- read.table("hs0.txt", sep = "", header = TRUE)
+str(dta)
+names(dta)[2] <- c("gender")
+
+# ~variable_name just like dta$X
+p <- ggvis(data = dta, x = ~math, y = ~science)
+layer_points(p)
+
+#
+ggvis(data = dta, x = ~math, y = ~science) %>%
+  layer_points() %>%
+  add_axis("x", title = "Math") %>%
+  add_axis("y", title = "Science")
+  
+#
+ggvis(data = dta, x = ~math, y = ~science, fill = ~ses) %>%
+  layer_points() %>%
+  add_axis("x", title = "Math") %>%
+  add_axis("y", title = "Science")
+  
+#
+dta$ses <- factor(dta$ses, levels = c("low", "middle", "high"))
+
+ggvis(data = dta, x = ~math, y = ~science, fill = ~ses) %>%
+  scale_ordinal("fill", range = c("lightblue","steelblue","blue")) %>%
+  layer_points() %>%
+  add_axis("x", title = "Math") %>%
+  add_axis("y", title = "Science")
+
+ggvis(data = dta, x = ~math, y = ~science,
+      fill = ~ses, stroke := "black") %>%
+  scale_ordinal("fill", range = c("lightblue","steelblue","blue")) %>%
+  layer_points() %>%
+  add_axis("x", title = "Math") %>%
+  add_axis("y", title = "Science")
+  
+ggvis(data = dta, x = ~math, y = ~science, fill = ~read) %>%
+  scale_numeric("fill", range = c("pink","blue")) %>%
+  layer_points() %>%
+  add_axis("x", title = "Math") %>%
+  add_axis("y", title = "Science")
+
+# interactivity
+p <- ggvis(data = dta, x = ~math, y = ~science,
+      fill = ~read, opacity := 0.5) %>%
+  scale_numeric("fill", range = c("pink","blue")) %>%
+  layer_points() %>%
+  add_axis("x", title = "Math") %>%
+  add_axis("y", title = "Science")
+
+# add new layer
+p %>% layer_model_predictions(model = "lm")
+p %>% layer_smooths()
+
+# add_tooltip
+p %>% add_tooltip(function(dta) dta$read)
+
+# input_select
+ggvis(dta, ~read, ~math, opacity := 0.6) %>%
+  layer_points(
+    fill = input_select(
+      choices = c("Gender" = "gender",
+                  "Race" = "race",
+                  "Social Economic Status" = "ses",
+                  "School Type" = "schtyp",
+                  "Program" = "prog"),
+      label = "Fill by",
+      selected = "gender",
+      map = as.name))
